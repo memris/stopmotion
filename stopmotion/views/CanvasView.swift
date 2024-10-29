f083634d393af871dfde7260c858da4abe7b82a0
memris <michurinaalyona@gmail.com>
2024-10-29 23:47:15 +0300

    undo,redo buttons,color picker added

--- stopmotion/views/CanvasView.swift
+++ stopmotion/views/CanvasView.swift
@@ -27,52 +27,126 @@ struct Line {
 }
 
 struct CanvasView: View {
-    @State private var selectionModeIndex: Int = 0
+    @State private var selectionModeIndex: Int? = nil
     @State private var lines: [Line] = []
+    private var paperImage: Image = Image("paper")
+    @State private var redoHistory: [Line] = []
+    @State private var selectedColor: Color = .black
+    @State private var isShowingColorPicker = false
 
     var body: some View {
         VStack {
-            Picker("Mode:", selection: $selectionModeIndex) {
-                Text("Draw mode").tag(0)
-                Text("Eraser Mode").tag(1)
-            }
-            .pickerStyle(.segmented)
-
-            Canvas { context, size in
-                for line in lines {
-                    var path = Path()
-                    path.addLines(line.points)
-
-                    if line.mode == .draw {
-                        context.blendMode = .normal
-                        context.stroke(path, with: .color(line.color), style: StrokeStyle(lineWidth: line.lineWidth))
-                    } else {
-                        context.blendMode = .destinationOut // .destinationOut для стирания
-                        context.stroke(path, with: .color(.white), style: StrokeStyle(lineWidth: line.lineWidth * 2))
+            Spacer()
+            HStack(spacing:20){
+                Button{
+                    if !lines.isEmpty {
+                        let lastAction = lines.removeLast()
+                        redoHistory.append(lastAction)
                     }
+                } label: {
+                    Image(systemName: "arrow.uturn.backward")
+                        .foregroundColor(.black)
+                        .font(.system(size: 20))
+                        .padding(9)
+                        .overlay(
+                            Circle()
+                                .stroke( .black, lineWidth: 2)
+                        )
                 }
-            }
-            .gesture(
-                DragGesture()
-                    .onChanged { dragValue in
-                        if lines.isEmpty {
-                            lines = [Line(color: .black, points: [dragValue.startLocation], mode: Mode(rawValue: selectionModeIndex)!, lineWidth: 5)]
-                        } else {
-                            let lastLine = lines[lines.count - 1]
+                Button{
+                    if !redoHistory.isEmpty {
+                        let lastRedoAction = redoHistory.removeLast()
+                        lines.append(lastRedoAction)
+                    }
+                } label: {
+                    Image(systemName: "arrow.uturn.right")
+                        .foregroundColor(.black)
+                        .font(.system(size: 20))
+                        .padding(9)
+                        .overlay(
+                            Circle()
+                                .stroke( .black, lineWidth: 2)
+                        )
+                }
+                ColorPicker("", selection: $selectedColor)
+                    .labelsHidden()
+             }
+         Spacer()
+            ZStack {
+                paperImage
+                    .resizable()
+                    .frame(width: 350, height: 600)
+                Canvas { context, size in
+                    for line in lines {
+                        var path = Path()
+                        path.addLines(line.points)
 
-                            if dragValue.startLocation != lastLine.points.first! || lastLine.mode != Mode(rawValue: selectionModeIndex)! {
-                                // Start a new line if start location is different or mode changed
-                                let newLine = Line(color: .black, points: [dragValue.startLocation], mode: Mode(rawValue: selectionModeIndex)!, lineWidth: 5) // Добавлен lineWidth
-                                lines.append(newLine)
-                            } else {
-                                // Append point to last line
-                                lines[lines.count - 1].points.append(dragValue.location)
-                            }
+                        if line.mode == .draw {
+                            context.blendMode = .normal
+                            context.stroke(path, with: .color(line.color), style: StrokeStyle(lineWidth: line.lineWidth))
+                        } else {
+                            context.blendMode = .destinationOut //для стирания
+                            context.stroke(path, with: .color(.white), style: StrokeStyle(lineWidth: line.lineWidth * 2))
                         }
                     }
-            )
-            .frame(width: 300, height: 400)
-            .border(Color.gray, width: 1)
+                }
+                
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragValue in
+                            if lines.isEmpty {
+                                if let selectionModeIndex = selectionModeIndex, let mode = Mode(rawValue: selectionModeIndex) {
+                                    lines = [Line(color: selectedColor, points: [dragValue.startLocation], mode: mode, lineWidth: 5)]
+                                }
+                               
+                            } else {
+                                let lastLine = lines[lines.count - 1]
+                                if let selectionModeIndex = selectionModeIndex {
+                                   
+                                    if dragValue.startLocation != lastLine.points.first! || lastLine.mode != Mode(rawValue: selectionModeIndex)! {
+                                        let newLine = Line(color: selectedColor, points: [dragValue.startLocation], mode: Mode(rawValue: selectionModeIndex)!, lineWidth: 5)
+                                        lines.append(newLine)
+                                    } else {
+                                        lines[lines.count - 1].points.append(dragValue.location)
+                                    }
+                                }
+                              
+                            }
+                        }
+                    
+                )
+                .frame(width: 350, height: 600)
+                
+            }
+            .cornerRadius(20)
+            Spacer()
+            HStack(spacing:20){
+                Button{
+                    selectionModeIndex = 0
+                } label: {
+                    Image(systemName: "pencil")
+                        .foregroundColor(selectionModeIndex == 0 ? Theme.accentColor : .black)
+                        .font(.system(size: 30))
+                        .padding(9)
+                        .overlay(
+                            Circle()
+                                .stroke(selectionModeIndex == 0 ? Theme.accentColor : .black, lineWidth: 3)
+                        )
+                }
+                Button{
+                    selectionModeIndex = 1
+                } label: {
+                    Image(systemName: "eraser")
+                        .foregroundColor(selectionModeIndex == 1 ? Theme.accentColor : .black)
+                        .font(.system(size: 27))
+                        .padding(9)
+                        .overlay(
+                            Circle()
+                                .stroke(selectionModeIndex == 1 ? Theme.accentColor : .black, lineWidth: 3)
+                        )
+                }
+            }
+            Spacer()
         }
     }
 }
