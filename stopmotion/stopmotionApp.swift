9e2bd4f02bd8f5dafc9be01f3cd99436119b0e21
memris <michurinaalyona@gmail.com>
2024-10-29 09:39:00 +0300

    Initial Commit

---
+++ stopmotion/stopmotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopmotionApp.swift
+//  stopmotion
+//
+//  Created by USER on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct stopmotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
